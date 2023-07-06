8cdc3df24c168ad2df465260ed57b2b972110889
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-07-06 16:39:37 +0545

    changes

---
+++ index.php
@@ -0,0 +1,383 @@
+<?php session_start();?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/silverBox.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <title>Arbor West Brampton | Singles & Towns - Register Today</title>
+
+    <meta name="title" content="Arbor West Brampton | Singles & Towns - Register Today">
+    <meta name="description" content="Arbor West by Caivan Communities is an upcoming pre construction singles & towns development in Bovaird Dr W & Mississauga Rd, Brampton. The community is set to launch in September 2023.">
+    <meta name="keywords" content="Arbor West Brampton , Arbor West, Arbor West Homes Brampton, Caivan Communities, Pre construction in brampton,  Singles and towns in Brampton, Pre consruction single and towns brampton">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="1 days">
+    <meta property="og:image" content="https://arborwesthome.ca/Arbor-West-Homes-Community.jpg"> 
+    <meta property="og:site_name" content="Arbor West" />
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+
+    <script type="application/ld+json">
+    {
+      "@context" : "https://schema.org",
+      "@type" : "WebSite",
+      "name" : "Arbor West",
+      "url" : "http://arborwesthome.ca/"
+    }
+  </script>
+</head>
+
+<body>
+
+    <header class="header-transparent" id="header">
+
+        <nav class="navbar navbar-expand-lg fixed-top" id="navbar">
+            <div class="container pt-2">
+                <a href="/" class="navbar-brand">
+                    <h1 class="navbar-brand">Arbor West</h1>
+                </a>
+                <a href="#Contact" class="btn btn-outline-light fss d-block d-md-none">
+                                    Register Now
+            </a>
+                <button class="d-block d-md-none btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" fill="white" />
+      </svg>
+    </button>
+                <div class="row me-0 row-cols-2 collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="col-12 d-flex justify-content-start justify-content-md-end">
+                        <ul class="navbar-nav my-2 mb-lg-0 align-items-start align-items-md-center">
+                            <li class="nav-item">
+                                <a href="#hero" class="nav-link scrollto" aria-current="page">
+              Home
+            </a>
+                            </li>
+                            <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#about">About</a>
+                            </li>
+
+                            <!-- <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#gallery">Renderings</a>
+                            </li> -->
+                            <!-- <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#plans">Plans & Pricing</a>
+                            </li> -->
+                            <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#location">Location</a>
+                            </li>
+
+
+                            <li class="nav-item px-md-2 d-none d-md-block">
+                                <a href="#contact" class="btn btn-outline-light btn-lg fss">
+                                    Register Now
+            </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section id="hero">
+        <div class="container">
+            <div class=" hero-container" >
+                <div >
+                <h1 class="main-titlee pb-0  mb-0 mt-2 mt-md-0" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">Arbor West<br>Brampton</span>
+                </h1>
+                <p class=" headline-textt mt-0 pt-3 text-white" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50"> Featuring Caivan's award-winning singles and towns.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-md-5 py-md-5" id = "about">
+        <div class="container mt-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
+            <h1 class=" main-title pt-md-5 pb-5  fw-bold ps-3 ps-md-0 d-flex justify-content-center d-flex justify-content-center align-items-center text-center">About Arbor West</h1>
+            <p class=" pb-3 px-3 textt d-flex justify-content-center align-items-center">Arbor West in Brampton is an upcoming pre construction singles & towns development in Bovaird Dr W & Mississauga Rd, Brampton, ON by Caivan Communities. The community is set to launch in September 2023.
+            <div class="list  textt  pt-2 px-3">
+                                <p class="mybot pb-md-0 pb-0 ">
+                                    Builder: Caivan Communities
+                                </p>
+
+                                <p class="mybot pb-md-0 pb-0 ">
+                                    Address:  Bovaird Dr W & Mississauga Rd, Brampton.
+                                </p>
+                                <p class="mybot pb-md-0 pb-0 ">
+                                Occupancy: TBD
+                                </p>
+                                <p class="mybot pb-md-0 pb-0 ">
+                                Pricing : Not available at the moment
+                                </p>
+
+</div>
+
+            </p>
+        </div>
+    </section>
+
+    <!-- <section>
+        <div class="">
+            <div class="container pt-3 " id="gallery">
+                <div class="my-md-5 my-3"></div>
+                <div class="pt-md-5 pt-5">
+                    <h2 class="fw-bold fs-1 pb-3 ps-3 ps-md-0 d-flex justify-content-center  brand-color">
+                        Models and Renderings
+                    </h2>
+                </div>
+                <div class="rounded-3">
+                    <div class="d-flex justify-content-between pb-3">
+                        <div class="row 
+                         row-cols-1 row-cols-sm-2 row-cols-lg-3 justify-content-center g-4 
+                        ">
+                            <div class="col">
+                                <div class="afte">
+                                    <img loading="lazy" src="images/southland-condos-1.jpeg" alt="Arbor West" class="img-fluid imghei">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="afte">
+                                    <img loading="lazy" src="images/southland-condos-2.jpeg" alt="Arbor West" class="img-fluid imghei">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div href="#" class="afte">
+                                    <img loading="lazy" src="images/the southlands.webp" alt="Arbor West" class="img-fluid imghei">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <section >
+        <div class="container pt-md-5 pt-5" id="location">
+            <div class="pt-md-5 pt-5"></div>
+            <div class="py-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
+                <h2 class="fw-bold fs-1 mb-0 text-center justify-content-center" >
+                    <span class="mx-1 pb-3">Arbor West - Location</span> </span>
+                </h2>
+                <span class="small-text text-center d-flex justify-content-center custom-underline pb-3">Bovaird Dr W & Mississauga Rd, Brampton.</span>
+                <div>
+                    <div class="bg-white p-1  d-flex justify-content-center ">
+                        <<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.0932701250504!2d-79.8296064!3d43.667030000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b149df88ce077%3A0xca68d71e9f15cfe0!2sMississauga%20Rd%20%26%20Bovaird%20Dr%20W%2C%20Brampton%2C%20ON%20L7A%200H5%2C%20Canada!5e0!3m2!1sen!2snp!4v1688632611297!5m2!1sen!2snp" width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-aos="fade-up" >
+        <div class="container pt-md-5 pt-5" id="location">
+            <div class="pt-md-5 "></div>
+            <div class="py-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
+                <h2 class="fw-bold fs-1 mb-0 text-center justify-content-center" data-aos="fade-up">
+                    <span class="mx-1 pb-3">Street View</span> </span>
+                </h2>
+                <span class="small-text text-center d-flex justify-content-center custom-underline pb-3 px-2">Walk around the neighbourhood near Arbor West Homes</span>
+                <div class="p-1  d-flex justify-content-center">
+                <iframe src="https://www.google.com/maps/embed?pb=!4v1688633711390!6m8!1m7!1scD-7bOChQBr-C-0BY17YIA!2m2!1d43.66687855630886!2d-79.82923515431882!3f20.021435068604447!4f-2.5495627739854996!5f0.7820865974627469" width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <span class="small-text text-center d-flex justify-content-center custom-underline pb-3">Note : The exact location of the project may vary from the street view shown here</span>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="pt-5 " id="contact">
+            <div class="pt-5 mt-5 " data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
+                <div class="row mx-0 " >
+                    <!-- <div class="text-center pt-3 ">
+                        <img src="images/empire-logo.png " class="img-fluid regoimg " alt="Empire Communities Logo ">
+                    </div> -->
+                    <h6 class="text-center fs-1 fw-bold pt-3 register " > Register today</h6>
+                    <div class="registertext px-4 d-flex justify-content-center align-items-center text-center ">
+                        <p>Register today and get the first update on Arbor West coming up in Brampton
+                        </p>
+                    </div>
+
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 mx-0 g-5 pb-md-5 ">
+                    <div class="col-md-3 "></div>
+                    <div class="col-md-6 px-md-5 ">
+
+                        <form action="contactForm.php" method="POST">
+                            <div class="row ">
+                                <div class="mb-3 "><input type="text" placeholder="Name" name="name" id="name" class="fields" required></div>
+
+                            </div>
+                            <div class="row row-cols-1 row-cols-sm-2 ">
+                                <div class="col ">
+                                    <div class="mb-3 "><input type="text" aria-describedby="emailHelp" placeholder="Phone" name="phone" id="phone" class="fields" required></div>
+                                </div>
+                                <div class="col ">
+                                    <div class="mb-3 "><input type="text" aria-describedby="emailHelp" placeholder="Email" name="email" id="email" class="fields" required></div>
+                                </div>
+                            </div>
+                            <div class="row ">
+                                <div class=" "><textarea name="message" id="message" placeholder="Enter your message" class="fields mess" required></textarea></div>
+                            </div>
+                            <div class="row ">
+                                <p class=" text-muted sm-text text-center mt-0 ">*I consent to receive future communications about Arbor West Homes. I understand I can opt out at anytime by sending an email.</p>
+                            </div>
+                            <div class="row ">
+                                <div class="col text-center ">
+                                    <!-- <input type="submit " onclick="sendMail() " value="Send " class="btn call-btn btn-sm p-2 px-4 btn-lg "> -->
+                                    <Button class="btn call-btn  p-2 px-4 btn-lg">Send</Button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div class="row-cols-md-3 "></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="mt-5 pt-3 ">
+        <img loading="lazy " src="images/line.png " alt="Line image for character " class="img-fluid foot-up-img " />
+    </div>
+    <footer class="footerr py-5 mb-4 ">
+        <div class="container ">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 pt-5 justify-content-between ">
+                <div class="col-md-5 my-3 fotwidth center-sm px-4 px-md-0 ">
+                    <p class=" ">
+                        <h1 class="navbar-brand-foot fs-4"><a href="https://homebaba.ca/brampton/arbor-west-community" target="_blank" class = "text-dark text-decoration-none">Arbor West </a></h1>
+                    </p>
+                    <span class=" justifyy ">
+                    We do not represent the builder. Information provided on this website is gathered from publicly available websites. The information might not be updated and could include inaccurate or outdated information such as location, pricing, availability, floor plans etc. To learn more about this condo please contact the builder directly. </span>
+                    <div class="list pt-md-5 pt-3 d-md-block d-none ">
+                        <p class=" ">
+                            Website designed by <a href="https://www.sixdesign.ca/ " target="_blank " class="text-dark text-decoration-none "><strong>SixDesign.</strong> </a>
+                        </p>
+
+                    </div>
+                </div>
+
+                <div class="col-md-4 py-md-5 py-5 center-sm px-md-5 px-5 ">
+                    <div>
+                        <h5 class="text-dark fw-bold mt-3 mb-3 ">About Arbor West</h5>
+                        <div class="list d-block ">
+                            <p class="mybot pb-md-0 pb-0 ">
+                                Builder : Caivan Communities
+                            </p>
+                            <p class="mybot pb-md-0 pb-0 ">
+                                Address : Bovaird Dr W & Mississauga Rd, Brampton.
+                            </p>
+                            <p class="mybot pb-md-0 pb-0 ">
+                                House Type : Singles and Towns
+                            </p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-3 mt-sm-0 pt-4 center-sm d-flex justify-content-around align-items-center text-center ">
+                    <img src="images/1621613861634.jpg" class="img-fluid footer-logo " alt="Caivan Communities ">
+
+
+                </div>
+            </div>
+
+            <div class="list mt-5 pt-5 d-md-none d-block center-sm px-4 ">
+                <p class=" ">
+                    Website designed by <a href="https://www.sixdesign.ca/ " target="_blank " class="text-dark text-decoration-none "><strong>SixDesign.</strong> </a>
+                </p>
+
+            </div>
+
+        </div>
+    </footer>
+
+
+
+    <script>
+        // Change navbar background color on scroll
+        window.addEventListener('scroll', function() {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > 0) {
+                navbar.classList.remove('transparent-navbar');
+                navbar.classList.add('solid-navbar', 'navbar-brand-sm');
+
+
+            } else {
+                navbar.classList.remove('navbar-brand-sm');
+                navbar.classList.remove('solid-navbar');
+                navbar.classList.add('transparent-navbar');
+
+            }
+        });
+    </script>
+     <script src="js/silverBox.min.js" ></script>
+     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+  AOS.init();
+</script>
+            
+            <?php
+        if(
+        isset($_SESSION['success'])
+        ){
+            ?>
+    <script type="text/javascript" async>
+       silverBox({
+	title: {
+		text: "Success",
+		alertIcon: "success",
+	},
+	text: "Your Message has been received.\n We'll get back to you soon.",
+});
+    </script>
+    <?php
+            unset($_SESSION['success']);
+        }
+
+        if(
+            isset($_SESSION['error'])
+            ){
+                ?>
+    <script type="text/javascript" async>
+           silverBox({
+	title: {
+		text: "Error",
+		alertIcon: "error",
+	},
+	text: "Your Message has not been received.",
+});
+    </script>
+    <?php
+                unset($_SESSION['error']);
+            }
+    ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
