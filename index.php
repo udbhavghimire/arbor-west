76c64e642b3d036bfcc0f908de71b962056d6d0a
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-07-16 14:30:33 +0545

    neighbourhood photo

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-    <title>Arbor West Brampton | Singles & Towns - Register Today</title>
+    <title> Arbor West Homes - Brampton | Register Today</title>
 
-    <meta name="title" content="Arbor West Brampton | Singles & Towns - Register Today">
+    <meta name="title" content="Arbor West Homes - Brampton | Singles & Towns - Register Today">
     <meta name="description" content="Arbor West by Caivan Communities is an upcoming pre construction singles & towns development in Bovaird Dr W & Mississauga Rd, Brampton. The community is set to launch in September 2023.">
     <meta name="keywords" content="Arbor West Brampton , Arbor West, Arbor West Homes Brampton, Caivan Communities, Pre construction in brampton,  Singles and towns in Brampton, Pre consruction single and towns brampton">
     <meta name="robots" content="index, follow">
@@ -238,6 +238,21 @@ This location is in Brampton. Nearby parks include Mount Pleasant Village Square
             </div>
         </div>
     </section>
+
+    <section data-aos="fade-up" >
+        <div class="container pt-md-5 pt-5" id="location">
+            <div class="pt-md-5 "></div>
+            <div class="py-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
+                <h2 class="fw-bold fs-1 mb-0 text-center justify-content-center" data-aos="fade-up">
+                    <span class="mx-1 pb-3">Discover The Community</span> </span>
+                </h2>
+                <span class="small-text text-center d-flex justify-content-center custom-underline pb-3 px-2">See the neighbourhood near Arbor West Homes</span>
+                </div>
+
+                <img src="images/arbor-west-neighbourhood.jpg" class ="img-fluid" alt="Arbor West Homes Neighbourhood ">
+        </div>
+    </section>
+
 
     <section class="mt-md-5 py-md-5" id = "faq">
         <div class="container mt-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50">
